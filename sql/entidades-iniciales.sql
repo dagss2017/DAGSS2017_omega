9648c01ab5af9f0126d4e98e9613517e75180291
edconde <ecnovoa@esei.uvigo.es>
2017-12-28 13:10:51 +0100

    Añadir inserts para pruebas

--- sql/entidades-iniciales.sql
+++ sql/entidades-iniciales.sql
@@ -18,7 +18,10 @@ INSERT INTO `MEDICO` (`ID`,`APELLIDOS`,`DNI`,`EMAIL`,`FECHAALTA`,`NOMBRE`,
                       `ULTIMOACCESO`,`CENTROSALUD_ID`)
    VALUES (2,'Gomez Gomez','11111111A','a@a.com','2017-11-11 01:04:42','Antonio',
            '11111','lUTQ2zg2voe4Z5OqpITFIjcBziNH10d6','988123456','MEDICO',
-           '2017-11-11 01:04:42',1);
+           '2017-11-11 01:04:42',1),
+		   (4, 'Over', '99999999A', 'bendover@yahoo.com', '1980-12-01 06:16:26', 'Bend',
+		   '9999999999', 'lUTQ2zg2voe4Z5OqpITFIjcBziNH10d6', '988264573', 'MEDICO',
+		   '2017-12-01 00:00:00', 1);
 
 -- Paciente con dni "22222222B", num. tarjeta sanitaria "2222222222", num seg. social "2222222222222" y contraseña "22222"
 INSERT INTO `PACIENTE` (`ID`,`APELLIDOS`,`DNI`,`EMAIL`,`FECHAALTA`,
@@ -28,7 +31,11 @@ INSERT INTO `PACIENTE` (`ID`,`APELLIDOS`,`DNI`,`EMAIL`,`FECHAALTA`,
    VALUES (3,'Benito Carmona','22222222B','b@b.com','2017-11-11 01:04:42',
            'Ana','2222222222222','2222222222',
            'auJIfVxFAN0IKkDVovGmzfUENiABIC5g','981123456','PACIENTE','2017-11-11 01:04:42',
-           '12345','C/. Benito, nº 2, 4º N','Coruña','Coruña',2);
+           '12345','C/. Benito, nº 2, 4º N','Coruña','Coruña',2),
+		   (5, 'Nitales', '88888888L', 'jorge@outlook.com', '2017-12-03 00:00:00',
+		   'Jorge', '7777777777777', '2222222222',
+		   'auJIfVxFAN0IKkDVovGmzfUENiABIC5g', '977456723', 'PACIENTE', '2017-12-03 04:12:15',
+		   '32890', 'Narnia 9, entresuelo A', 'Narnia town', 'Narnia', 2);
 
 -- Cita con fecha "2017-11-23" y hora "09:00:00"
 INSERT INTO `CITA`(`ID`,`DURACION`,`ESTADO`,`FECHA`,`HORA`,`MEDICO_ID`,`PACIENTE_ID`)
@@ -41,4 +48,13 @@ INSERT INTO `FARMACIA`(`ID`,`FECHAALTA`,`NIF`,`NOMBREFARMACIA`,
    VALUES (4,'2017-11-11 01:04:42','33333333C','Farmacia de prueba',
            '/QpUw+ZRH3ndoNb3N4gRpT5cz0C7pT9v','FARMACIA','2017-11-11 01:04:42',
            '12345','C/. Farmacia, nº 2, 4º N','Coruña','Coruña');
+		   
+-- Medicamentos de ejemplo
+INSERT INTO `MEDICAMENTO` (`ID`, `FABRICANTE`, `FAMILIA`, `NOMBRE`, `NUMERODOSIS`, `PRINCIPIOACTIVO`) VALUES
+(1, 'Bayern', 'Familia de Bayern', 'Hard Drugs', 3, 'Cocaine'),
+(2, 'IbuprofenPRO', 'Familia de IbuprofenPRO', 'DailyIbuprofen 3.0', 2, 'Codeina');
 
+-- Prescripción de ejemplo
+INSERT INTO `PRESCRIPCION` (`ID`, `DOSIS`, `FECHAFIN`, `FECHAINICIO`, `INDICACIONES`, `MEDICAMENTO_ID`, `MEDICO_ID`, `PACIENTE_ID`) VALUES
+(1, 3, '2018-01-26', '2017-12-01', 'Tomar con mucho cuidado la pastilla. Ha causado accidentes graves.', 1, 2, 3),
+(2, '2', '2018-01-15', '2017-12-30', 'No se drogue.', '2', '2', '3');
