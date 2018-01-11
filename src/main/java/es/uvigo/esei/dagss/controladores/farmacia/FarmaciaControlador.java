9109ccfc4d82ab6b6aafb6c91c245e293e5fe216
0xkush <camartinez@esei.uvigo.es>
2018-01-11 19:19:20 +0100

    historia-usuario #2;todo:recetas paciente

--- src/main/java/es/uvigo/esei/dagss/controladores/farmacia/FarmaciaControlador.java
+++ src/main/java/es/uvigo/esei/dagss/controladores/farmacia/FarmaciaControlador.java
@@ -5,7 +5,9 @@ package es.uvigo.esei.dagss.controladores.farmacia;
 
 import es.uvigo.esei.dagss.controladores.autenticacion.AutenticacionControlador;
 import es.uvigo.esei.dagss.dominio.daos.FarmaciaDAO;
+import es.uvigo.esei.dagss.dominio.daos.PacienteDAO;
 import es.uvigo.esei.dagss.dominio.entidades.Farmacia;
+import es.uvigo.esei.dagss.dominio.entidades.Paciente;
 import es.uvigo.esei.dagss.dominio.entidades.TipoUsuario;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -26,12 +28,20 @@ public class FarmaciaControlador implements Serializable {
     private Farmacia farmaciaActual;
     private String nif;
     private String password;
+    private String query;
+    private String tipoQuery;
+    
+    private Paciente pacienteActual;
 
     @Inject
     private AutenticacionControlador autenticacionControlador;
 
     @EJB
     private FarmaciaDAO farmaciaDAO;
+    
+    @EJB
+    private PacienteDAO pacienteDAO;
+
 
     /**
      * Creates a new instance of AdministradorControlador
@@ -62,6 +72,30 @@ public class FarmaciaControlador implements Serializable {
     public void setFarmaciaActual(Farmacia farmaciaActual) {
         this.farmaciaActual = farmaciaActual;
     }
+    
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getTipoQuery() {
+        return tipoQuery;
+    }
+
+    public void setTipoQuery(String tipoQuery) {
+        this.tipoQuery = tipoQuery;
+    }
+    
+    public Paciente getPacienteActual() {
+        return pacienteActual;
+    }
+
+    public void setPacienteActual(Paciente pacienteActual) {
+        this.pacienteActual = pacienteActual;
+    }
 
     private boolean parametrosAccesoInvalidos() {
         return ((nif == null) || (password == null));
@@ -87,5 +121,20 @@ public class FarmaciaControlador implements Serializable {
             }
         }
         return destino;
+    }
+    
+    public String doBuscarPaciente() {
+        switch(tipoQuery) {
+            case "1":
+                pacienteActual = pacienteDAO.buscarPorDNI(query);
+                break;
+            case "2":
+                pacienteActual = pacienteDAO.buscarPorTarjetaSanitaria(query);
+                break;
+            case "3":
+                pacienteActual = pacienteDAO.buscarPorNumeroSeguridadSocial(query);
+                break;
+        }
+        return "/farmacia/privado/receta_paciente";
     }
 }
